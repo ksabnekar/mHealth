b5186452f4c9d9c5ea309f561c53fde860da4741
snavelycoreyt <csnavely@unomaha.edu>
2021-07-02 11:29:17 -0500

    Corey update pushes

--- mhealth/settings.py
+++ mhealth/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test',
         'USER': 'postgres',
-        'PASSWORD': 'Ch@mpion92)',
+        'PASSWORD': '!QAZ1qaz',
         'HOST': 'localhost',
         'PORT': '5432',
     }
